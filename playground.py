19518f508cf328f36cfe90776974385288c7899a
PDHS lab <pdhslab@dsusd.k12.ca.us>
2018-04-20 10:43:42 -0700

    updated playground.py

--- playground.py
+++ playground.py
@@ -1,8 +1,13 @@
+import time
+
 n = 21971
 e = 131
+d = 17867
 
 message = 'Hello World!'
 encrypted_message = ''
+decrypted_message =''
+start = time.time()
 
 #########Encryption#########
 for x in message:
@@ -13,3 +18,73 @@ for x in message:
 
 print encrypted_message
     
+#########Decryption#########
+for t in encrypted_message:
+    numerize = ord(t)
+    decrypt = pow(numerize, d, n)
+    denumerize = chr(decrypt)
+    decrypted_message += denumerize
+    
+print decrypted_message
+end = time.time()
+
+print(end - start)
+
+encrypted_message2 = ''
+decrypted_message2 = ''
+LUT_encryption = dict()
+LUT_decryption = dict()
+
+
+start = time.time()
+#########Encryption v2#########
+for x in message:
+    if x in LUT_encryption:
+        encrypted_message2 += LUT_encryption[x]
+    else:
+        numerize = ord(x)
+        encrypt = pow(numerize, e, n)
+        denumerize = unichr(encrypt)
+        encrypted_message2 += denumerize
+        LUT_encryption[x] = denumerize
+
+print encrypted_message2
+
+
+#########Encryption v2#########
+for t in encrypted_message2:
+    if t in LUT_decryption:
+        decrypted_message2 += LUT_decryption[t]
+    else:
+        numerize = ord(t)
+        decrypt = pow(numerize, d, n)
+        denumerize = chr(decrypt)
+        decrypted_message2 += denumerize
+        LUT_decryption[t] = denumerize
+
+print decrypted_message2
+end = time.time()
+
+print(end - start)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
